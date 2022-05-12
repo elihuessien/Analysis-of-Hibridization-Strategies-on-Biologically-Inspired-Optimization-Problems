e3a91b1ec1b8d08e6518ece3856f790876943f84
elihuessien <elihuessien@gmail.com>
2022-05-12 18:07:54 +0100

    dissertation writeup 1 third complete

--- Code/R/Experiment3/C.R
+++ Code/R/Experiment3/C.R
@@ -3,9 +3,9 @@ library(pracma)
 library(dplyr)
 library(datasets)
 
-path1 <- "C:/Users/C14460702/Dissertation/Data/Results/Experiment 3/B/PSO.txt"
-path2 <- "C:/Users/C14460702/Dissertation/Data/Results/Experiment 3/B/MPSO.txt"
-path4 <- "C:/Users/C14460702/Dissertation/Data/Results/Benchmark/Size - 10/GreedyOptimizer.txt"
+path1 <- "C:/Users/C14460702/Dissertation/Data/Results/Experiment 3/C/PSO.txt"
+path2 <- "C:/Users/C14460702/Dissertation/Data/Results/Experiment 3/C/MPSO.txt"
+path3 <- "C:/Users/C14460702/Dissertation/Data/Results/Benchmark/Size - 10/GreedyOptimizer.txt"
 
 pso.data <- read.table(path1, header=FALSE, sep=",", dec=".")
 mpso.data <- read.table(path2, header=FALSE, sep=",", dec=".")
@@ -15,3 +15,79 @@ benchmark <- read.table(path3, header=FALSE, sep=",", dec=".")
 
 
 # Analyzing Averages
+pso.avg <- colMeans(pso.data)
+mpso.avg <- colMeans(mpso.data)
+
+ID <- c()
+Value <- c()
+Class <- c()
+for(i in 1:nrow(pso.data)){
+  ID <- c(ID, i)
+  Value <- c(Value, pso.avg[i])
+  Class <- c(Class, "(w - 80%) PSO")
+  
+  ID <- c(ID, i)
+  Value <- c(Value, mpso.avg[i])
+  Class <- c(Class, "(w - 80%) MPSO (30% - 70%, 50%)")
+  
+  ID <- c(ID, i)
+  Value <- c(Value, rowMeans(benchmark))
+  Class <- c(Class, "Greedy Optimizer")
+}
+
+
+
+AVG.df <- data.frame(ID, Value, Class)
+
+ggplot(AVG.df, mapping = aes(x = ID, y = Value, colour = Class)) +
+  geom_line(size = 1) +
+  labs(title = "Average GBest per Iteration",
+       x="Iteration", colour = "Algorithm")
+
+
+
+
+# Analyze AUC
+pso.auc <- trapezoidal(pso.data)
+mpso.auc <- trapezoidal(mpso.data)
+ID <- c()
+Value <- c()
+Class <- c()
+for(i in 1:nrow(pso.data)){
+  ID <- c(ID, i)
+  Value <- c(Value, pso.auc[i])
+  Class <- c(Class, "PSO")
+  
+  ID <- c(ID, i)
+  Value <- c(Value, mpso.auc[i])
+  Class <- c(Class, "MPSO")
+}
+
+AUC.df <- data.frame(ID, Value, Class)
+
+ggplot(AUC.df, aes(x = ID, y = Value, colour = Class)) +
+  geom_point() +
+  labs(title = "Learning Rate (AUC)", x="Map IDs", colour = "Algorithm")
+
+ggplot(AUC.df, aes(x = Class, y = Value, colour = Class)) +
+  geom_boxplot(outlier.shape = NA, ) +
+  guides(color ="none") +
+  labs(title = "Learning Rate (AUC)", x="Algorithm")
+
+ggplot(AUC.df, aes(x = Value, fill = Class, alpha = 1)) +
+  geom_histogram(position = "identity") +
+  guides(alpha ="none") +
+  labs(title = "Learning Rate (AUC)", x="Value", fill = "Algorithm")
+
+wilcox.test(Value ~ Class, data = AUC.df)
+
+
+
+
+
+# Clean Up
+rm(path1, path2, path3)
+rm(pso.data, mpso.data, benchmark)
+rm(pso.avg, mpso.avg, AVG.df)
+rm(pso.auc, mpso.auc, AUC.df)
+rm(ID, i, Value, Class)
